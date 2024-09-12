7c7c5194afe73f9bdff351945a08780c1ac6bffe
David Antonio López Pérez <230i0063@martineztorre.tecnm.mx>
2024-09-12 13:41:00 -0600

    Add files via upload

---
+++ Codigos/mi_lista = [1, 2, 3, 4, 5].py
@@ -0,0 +1,12 @@
+mi_lista = [1, 2, 3, 4, 5]
+
+
+print("Mi lista:", mi_lista)
+
+mi_lista.append(6)
+print("Lista después de agregar un elemento:", mi_lista)
+
+print("El primer elemento de la lista es:", mi_lista[0])
+
+mi_lista.remove(3)
+print("Lista después de eliminar un elemento:", mi_lista)
